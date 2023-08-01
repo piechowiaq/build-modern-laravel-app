460b09aa8b330556dcebae40cc7a156db76022d2
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-08-01 21:13:09 +0200

    Pagination

---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+defineProps({
+    links: Array
+})
+
+
+</script>
+
+
+<template>
+
+    <div>
+        <Component
+            :is="link.url ? 'Link' : 'span'"
+            v-for="link in links"
+            :href="link.url" v-html="link.label"
+            class="px-1"
+
+            :class="link.url ? '' : 'text-gray-500'"
+        />
+
+    </div>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
