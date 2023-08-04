dbaaf78b85f5a6c76cd5db0d41f1feb553db9d6d
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-08-04 22:05:56 +0200

    Better Performance With Throttle and Debounce

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -3,6 +3,7 @@
 import Pagination from "@/Shared/Pagination.vue";
 import { ref, watch } from "vue";
 import { router, Link } from "@inertiajs/vue3"
+import debounce from "lodash/debounce"
 
 
 let props = defineProps({
@@ -12,13 +13,9 @@ let props = defineProps({
 
 let search = ref(props.filters.search);
 
-watch(search, value => {
-    router.get('/users', { search: value}, {
-        preserveState: true,
-        replace: true
-    })
-
-})
+watch(search, debounce(function (value) {
+    router.get('/users', { search: value}, { preserveState: true, replace: true }) ;
+}, 300));
 
 </script>
 <template>
